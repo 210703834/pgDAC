1a87c31e429c445885d47cb58b16cf5ca35d7e2f
sanket <sanketpalkar18@gmail.com>
2021-10-06 15:10:48 +0530

    all files added

---
+++ average.c
@@ -0,0 +1,10 @@
+//pgm is to find average of two numbers
+#include<stdio.h>
+int main()
+{
+    int no1=20,no2=40;
+    float average;
+    average=(no1+no2)/2;
+    printf("average of %d and %d is:%f",no1,no2,average);
+    return 0;
+} 
